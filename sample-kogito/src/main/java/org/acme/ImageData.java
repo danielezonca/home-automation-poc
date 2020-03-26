ea8288a81d2419cee016d7e99e5a6a945fd7b4b6
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-03-26 16:32:06 +0100

    connects correctly to kafka

--- sample-kogito/src/main/java/org/acme/ImageData.java
+++ sample-kogito/src/main/java/org/acme/ImageData.java
@@ -2,10 +2,17 @@ package org.acme;
 
 public class ImageData {
     private String data;
+    public ImageData() {
+    }
     public ImageData(String data) {
         this.data = data;
     }
     public String getData() {
         return data;
     }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    
 }
