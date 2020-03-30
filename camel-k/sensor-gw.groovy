fbb95afefd8d1aa9dfd02148eadefbaecd42b643
lburgazzoli <lburgazzoli@gmail.com>
2020-03-30 16:18:38 +0200

    Add basic camel-k services

---
+++ camel-k/sensor-gw.groovy
@@ -0,0 +1,15 @@
+//
+// kamel run --dev camel-k/sensor-gw.groovy
+//
+rest {
+    post {
+        path '/sensor/{id}'
+        consumes 'application/json'
+        produces 'application/json'
+        to 'direct:sensor'
+    }
+}
+
+from('direct:sensor')
+  .to('log:sensor?showAll=true&multiline=true')
+  .to('knative:endpoint/notifier')
