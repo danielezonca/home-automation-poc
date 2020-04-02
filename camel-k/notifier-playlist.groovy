b4ab402fe7c1f56ff9d0d8ed2ce6d18c1c8f8328
lburgazzoli <lburgazzoli@gmail.com>
2020-04-02 17:26:13 +0200

    Update camel-k services

--- camel-k/notifier-playlist.groovy
+++ camel-k/notifier-playlist.groovy
@@ -3,14 +3,25 @@
 //
 import org.apache.camel.component.telegram.TelegramMediaType
 import org.apache.camel.component.telegram.TelegramConstants
+import org.apache.camel.model.dataformat.JsonLibrary
 
 from('knative:endpoint/notifier-playlist')
   .convertBodyTo(String.class)
-  .to('telegram:bots?chatId={{telegram.chat-id}}')  
-  .removeHeaders('*')
-  .setBody().constant(null)
-  .to('https://source.unsplash.com/1600x900/?QUERY')
-  .convertBodyTo(byte[].class)
-  .setHeader(TelegramConstants.TELEGRAM_MEDIA_TYPE).constant(TelegramMediaType.PHOTO_JPG)
-  .to('telegram:bots?chatId={{telegram.chat-id}}')
+  .unmarshal().json(JsonLibrary.Jackson)
+  .step()
+    .removeHeaders('Camel*')
+    .removeHeaders('Host')
+    .setHeader('ImageQuery').simple('${body[query]}')
+    .setBody().simple('${body[message]}')
+    .to('telegram:bots?chatId={{telegram.chat-id}}')  
+  .end()
+  .step()  
+    .removeHeaders('Camel*')
+    .removeHeaders('Host')
+    .setBody().constant(null)
+    .toD('https://source.unsplash.com/1600x900/?${header[ImageQuery]}')
+    .convertBodyTo(byte[].class)
+    .setHeader(TelegramConstants.TELEGRAM_MEDIA_TYPE).constant(TelegramMediaType.PHOTO_JPG)
+    .to('telegram:bots?chatId={{telegram.chat-id}}')
+  .end()
   .convertBodyTo(String.class)
